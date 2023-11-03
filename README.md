c98b60ac9b2d5bbb8840638f4564ccf248bdadf3
Rakshith-Raj08 <rakshith2002raj@gmail.com>
2023-11-03 13:33:32 +0530

    uploaded

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# React-Portfolio-Project
+# React Portfolio Website
+
+Welcome to my portfolio website! This is where I showcase my skills, projects, and experience to the world. Feel free to explore and get to know me better.
+
+## Technologies Used
+
+- React
+- React Router
+- HTML
+- CSS
+
+## Installation
+
+Clone the repository:
+
+   ```bash
+   git clone https://github.com/AswinPKumar01/react-portfolio-project.git
+   cd portfolio-website
---
+++ src/content_option.js
@@ -0,0 +1,142 @@
+const logotext = "ASWIN";
+const meta = {
+    title: "Aswin P Kumar",
+    description: "I'm Aswin P Kumar, Student at VIT Bhopal University",
+};
+
+const introdata = {
+    title: "I'm Aswin P Kumar",
+    animated: {
+        first: "Full Stack Developer",
+        second: "Application Developer",
+        third: "AIML Enthusiast",
+    },
+    description: "Currently a Third Year undergrad at VIT Bhopal, with a passion for creating pixel-perfect websites and cutting-edge algorithms",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Who I am",
+    aboutme: "I'm Aswin P Kumar, a third-year undergraduate at VIT Bhopal University, majoring in Computer Science and Engineering. I hail from the serene backwaters of Alappuzha in Kerala. I'm a tech enthusiast with a penchant for MERN full stack development, and I've been known to Flutter around, creating sleek mobile apps with Kotlin and Dart. I've got a crush on Machine Learning and AI, and Data Analysis is my secret indulgence"
+    };
+const worktimeline = [{
+        jobtitle: "Mern Full Stack Developer",
+        where: "Ethnus Training Centre",
+        date: "Aug '23 - Ongoing",
+    },
+    {
+        jobtitle: "Machine Learning Intern",
+        where: "Rinex Education Centre",
+        date: "Nov '22 - Jan '23",
+    },
+    {
+        jobtitle: "Web Scraper Intern",
+        where: "Plypicker Ecom Website",
+        date: "Aug '22 - Jul '23",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "Flutter",
+        value: 90,
+    },
+    {
+        name: "Kotlin",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Will be updated soon",
+        description: ":))",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Project 1",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Project 2",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Project 3",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Project 4",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Project 5",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Project 6",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Project 7",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Project 8",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aswinpkumar03@gmail.com",
+};
+
+const socialprofils = {
+    github: "https://github.com/AswinPKumar01",
+    linkedin: "https://www.linkedin.com/in/aswinpkumarvit/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
